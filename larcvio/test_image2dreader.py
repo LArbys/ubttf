8f430f41fffcea46b2a3385901b46c72ed2e64ba
Taritree Wongjirad <taritree@nunet.lns.mit.edu>
2016-06-25 06:08:00 -0400

    image2dreader works with 3 planes

--- larcvio/test_image2dreader.py
+++ larcvio/test_image2dreader.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     
     tfsession = tf.Session()
     feature_batch, label_batch = reader.startQueue(tfsession, batch_size)
-    time.sleep(1)
 
     print [reader.batch_size,reader.cols,reader.rows,reader.nchs]
 
@@ -25,20 +24,23 @@ if __name__ == "__main__":
     with tf.name_scope('image_dump'):
 
         # reshape
-        reshaped_imgs = tf.reshape( feature_batch, [reader.batch_size,reader.rows,reader.cols,reader.nchs] )
+        reshaped_imgs = tf.reshape( feature_batch, [reader.batch_size,reader.nchs,reader.rows,reader.cols] )
+
+        # permute (slow)
+        permute_imgs = tf.transpose( reshaped_imgs, perm=[0,2,3,1] )
 
         # split labels
         split_labels = tf.split( 0, reader.batch_size, label_batch )
 
         # summaries
-        plane_sums = []
         for plane in planes:
-            plane_sums.append( tf.image_summary( 'plane%d_img'%(plane), reshaped_imgs, max_images=reader.batch_size ) )
+            tf.image_summary( 'plane%d_img'%(plane), permute_imgs, max_images=reader.batch_size )
         for ibatch in range(0,reader.batch_size):
             tf.scalar_summary( "sum_label_%d"%(ibatch), split_labels[ibatch][0] )
 
     # Merge summary ops
     summary_ops = tf.merge_all_summaries()
+    print "Summary ops: ",summary_ops
     
     # startup a tensorflow session
     summ_dir = '/tmp/larcvio_ex_'+getpass.getuser()
@@ -55,13 +57,18 @@ if __name__ == "__main__":
             #imgs, labels = tfsession.run( [feature_batch, label_batch] )
             #print imgs.shape, labels.shape, labels
 
-            out_reshape, sum_event, labels = tfsession.run( [reshaped_imgs,summary_ops,label_batch] )
+            if summary_ops is not None:
+                out_reshape, sum_event, labels = tfsession.run( [permute_imgs,summary_ops,label_batch] )
+            else:
+                out_reshape,labels = tfsession.run( [permute_imgs,label_batch] )
             print "output shape: ",out_reshape.shape
             print "output labels: ",labels
-            summary_writer.add_summary( sum_event )
+            if summary_ops is not None:
+                summary_writer.add_summary( sum_event )
 
             # double check by outputing images
             for ibatch in range(0,reader.batch_size):
+                #imgout = np.transpose(out_reshape[ibatch,:,:,:],(1,2,0))*100.0
                 imgout = out_reshape[ibatch,:,:,:]*100.0
                 print "write out image ",ibatch
                 cv2.imwrite("imgout_%d.png"%(ibatch),imgout)
