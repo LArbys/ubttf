ebd4003e507d29a10d6811a175fe214f9ac241b3
Taritree Wongjirad <taritree@nunet.lns.mit.edu>
2016-06-25 20:27:48 -0400

    clean up and comment a bit

--- bvlc_alexnet/imgtest_bvlc_alexnet.py
+++ bvlc_alexnet/imgtest_bvlc_alexnet.py
@@ -4,8 +4,6 @@ import numpy as np
 import tensorflow as tf
 from Image2DReader import Image2DReader
 from BVLCAlexNetModel import BVLCAlexNetModel
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import cv2
 from caffe_classes import class_names # list of names
 
@@ -13,19 +11,16 @@ from caffe_classes import class_names # list of names
 
 if __name__ == "__main__":
 
-    print cv2.__version__
-    
+    # LOAD SAMPLE IMAGES
+    # we use OpenCV which uses BGR, convert to RGB
     # cv2.cvtColor(srcBGR, cv2.COLOR_BGR2RGB) # why!!!
     img_dog   = cv2.cvtColor( cv2.imread( "dog.png", cv2.IMREAD_COLOR ), cv2.COLOR_BGR2RGB )
     img_quail = cv2.cvtColor( cv2.imread( "quail227.JPEG", cv2.IMREAD_COLOR ), cv2.COLOR_BGR2RGB )
 
-    # switch to RGB
-    #img_dog   = np.transpose( img_dog, (2,1,0) )
-    #img_quail = np.transpose( img_quail, (2,1,0) ) 
-    print img_dog.dtype, img_dog.shape
+    # Load mean pixel values for each channel , RGB
+    mean_rgb = np.array( [104, 117, 123], dtype=np.float32 ) # from https://github.com/BVLC/caffe/wiki/Models-accuracy-on-ImageNet-2012-val
 
-    mean_rgb = np.array( [104, 117, 123], dtype=np.float32 ) # https://github.com/BVLC/caffe/wiki/Models-accuracy-on-ImageNet-2012-val
-
+    # Make mean images, also change type to float32
     input_dog = np.zeros( img_dog.shape, dtype=np.float32 )
     input_quail = np.zeros( img_quail.shape, dtype=np.float32 )
     for i in range(0,3):
@@ -38,31 +33,43 @@ if __name__ == "__main__":
     print "img shape: ",input_dog.shape
     print "vec shape: ",vecshape
 
+    # Placeholder variables for images
     image_input_node = tf.placeholder( tf.float32, name="input", shape=[1,vecshape] )
     label_input_node = tf.placeholder( tf.int32,   name="label", shape=[] )
 
+    # Load BVLC AlexNet Model
     caffe_weightfile = 'bvlc_alexnet.npy'
     model = BVLCAlexNetModel( image_input_node, label_input_node, input_dog.shape, num_classes, caffe_weightfile=caffe_weightfile )
     
+    # Make operation to initialize variables
     init_op = tf.initialize_all_variables()
+    
+    # Start session
     tfsession = tf.Session()
 
+    # Make vector for input
     net_input = np.zeros( image_input_node.get_shape().as_list(), dtype=np.float32 )
+
+    # Choose image: dog or quail
     #net_input[0,:] = input_dog.flatten()
     net_input[0,:] = input_quail.flatten()
 
+    # Do initialization
     start_init = time.time()
     tfsession.run( init_op )
     print "Initialized in ",time.time()-start_init
 
-    conv1b_init = tfsession.run( model.conv1_b.initial_value )
-    # check of initialization lambdas
+    # DEBUG: check of initialization lambdas, did they load the right values?
+    #conv1b_init = tfsession.run( model.conv1_b.initial_value )
     #print "initial conv1b values: ",conv1b_init
     #print model.net_data["conv1"][1]
 
+    # Forward pass on image
     start_forward = time.time()
     outprob = tfsession.run( model.prob, feed_dict={ image_input_node:net_input,
                                                      label_input_node:1 } )
+
+    # How did we do? (stupidest top-K algorithm)
     print "Ran one sample in ",time.time()-start_forward
     for k in range(0,5):
         maxk = np.argmax( outprob[0] )
